d0eeddf7578ec7a8cf761148d3a295c0cc12688e
indy6678 <indy6678@gmail.com>
2022-08-03 17:52:31 -0500

    added intern question list

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const inquirer = require('inquirer');
 const managerList = require('./src/manager');
 const engineerList = require('./src/engineer');
+const internList = require('./src/intern');
 
 const promptManager = () => {
     return inquirer.prompt(managerList)
@@ -11,13 +12,21 @@ const promptEngineer = () => {
     return inquirer.prompt(engineerList);
 }
 
+const promptIntern = () => {
+    return inquirer.prompt(internList)
+};
+
 // function call to initialize app
 
 promptManager()
     .then(answers => {
-        console.log(answers)
+        console.log(answers);
     })
     .then(promptEngineer)
+    .then(answers => {
+        console.log(answers);
+    })
+    .then(promptIntern)
     .then(answers => {
         console.log(answers);
     })
